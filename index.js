75f953a84b3d9034d025d02ca6cf220a908f8cc0
Marcio <marcio@localhost.localdomain>
2020-02-05 11:36:35 -0300

    Query e Route params

--- index.js
+++ index.js
@@ -2,10 +2,23 @@ const express = require('express');
 
 const server = express();
 
-//localhost:3000/teste
-server.get('/teste', (req, res) =>{
-  return res.json({message:'Hello World'});
-})
+//Query params = ?teste=1  exemplo: http://localhost:3000/teste?nome=marcio
+//Route params = /users/1  exemplo: http://localhost:3000/users/2
+//Request body = { "name": "email", "email": "email@email.com.br" }
+
+/*Query params
+server.get('/users', (req, res) =>{
+  const nome = req.query.nome;
+  
+  return res.json({message:`Hello ${nome}`});
+})*/
+
+/*Route params
+server.get('/users/:id', (req, res) =>{
+  const { id }= req.params;
+  
+  return res.json({message:`Buscando o usuário ${id}`});
+})*/
 
 server.listen(3000);
 
